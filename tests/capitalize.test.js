ca7c2faab33dc49924c90e14fd163bb38dfcfa10
Nikita <nik_voronkov_1996@mail.ru>
2024-07-08 14:06:34 +0700

    Добавил утверждение assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,14 +1,9 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
+import { strict as assert } from 'assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
-
-console.log('Все тесты пройдены!');
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'), 'Hello');
+assert.strictEqual(capitalize('hello'), 'hello'); // err
